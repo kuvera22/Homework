deaac61a0a1a82d0519d11c8e70dd970dae2e2b5
kuvera22 <kuvera22@mail.ru>
2016-12-03 17:53:07 +0300

    Домашняя работа на 03.12.2016

---
+++ Разметка школа/app/src/main/java/com/example/verka/hwschphone/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.example.verka.hwschphone;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
